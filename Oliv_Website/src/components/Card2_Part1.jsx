a50d814d781a2e13c870e2ff49ad3c3c366eeebd
SaloniGite <salonigite2003@gmail.com>
2025-02-02 23:14:24 +0530

    third commit

---
+++ Oliv_Website/src/components/Card2_Part1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card2 = () => {
+  return (
+    <div>
+      <h2></h2>
+      <div>
+        <h2>Our Approach to </h2>
+        <h2>Branding</h2>
+      </div>
+      <button>About Oliv </button>
+    </div>
+  )
+}
+
+export default Card2
